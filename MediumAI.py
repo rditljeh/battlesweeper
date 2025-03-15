d7764db2cb8b0449642e8bbad15050ab0851cdf2
CharlesSailors <131941112+CharlesSailors@users.noreply.github.com>
2025-03-15 03:17:04 -0500

    Medium changes: set selected to 0 probability all cells have possibilities medium not pick its own mines I believe the print statements are not in good order to read data properly, and algorithm needs to be thoroughly tested. (will crash at end until game over is complete)

--- MediumAI.py
+++ MediumAI.py
@@ -163,10 +163,23 @@ def AI_checks_100s(board):
     #return x, y
 
 def AI_chooses_0s_but_not_100s(Board):
+    reset_probabilities(Board)
     AI_checks_100s(Board)
     AI_checks_0s(Board)
+    print("before check")
+    check_rest_probabilities(Board)
+    print("after check")
     zeroChance, definiteChance = checkProbabilities(Board)
     selections = checks_all_possible_selections(Board)
+
+    AIPlacedBombs = Board.AIplaced  # checkKnown(Board)
+    for i in AIPlacedBombs:
+        print("probability:", Board.cell_dict[f"{i[0]},{i[1]}"]["probability"])
+        Board.cell_dict[f"{i[0]},{i[1]}"]["probability"] = 1
+        print("probability:", Board.cell_dict[f"{i[0]},{i[1]}"]["probability"])
+        if i in selections:
+            selections.remove(i)
+
     print("zeroChance:", zeroChance)
     print("definiteChance:", definiteChance)
     print("selectioncs:", selections)
@@ -181,15 +194,28 @@ def AI_chooses_0s_but_not_100s(Board):
 
     #if no such cell exists above, chooses a random cell until it finds one that is unselected without 100% chance
     else:
-        print("no zero chance yet")
+
         for i in definiteChance:
             if i in selections:
                 selections.remove(i)
-        randomCell = random.randint(0, len(selections) - 1)
-        x, y = selections[randomCell]
-        if x == 0 and y == 0:
-            x = 1
+        lowest_possibility = .99
+        lowest_possibility_array = []
+        for i in selections:
+            print("no zero chance yet, i[0], i[1]:", i[0], i[1])
+            if Board.cell_dict[f"{i[0]},{i[1]}"]["probability"] < lowest_possibility:
+                lowest_possibility = Board.cell_dict[f"{i[0]},{i[1]}"]["probability"]
+        for i in selections:
+            if Board.cell_dict[f"{i[0]},{i[1]}"]["probability"] == lowest_possibility:
+                print("append: ", Board.cell_dict[f"{i[0]},{i[1]}"]["probability"], "to", i[0], ":", i[1],)
+                lowest_possibility_array.append([i[0],i[1]])
+        randomCell = random.randint(0, len(lowest_possibility_array) - 1)
+        print("array:", lowest_possibility_array)
+        print("randomCell:", randomCell)
+        print("this is lowest:", lowest_possibility_array[randomCell])
+        x, y = lowest_possibility_array[randomCell]
+        print("got here with:", x, y)
 
+    Board.cell_dict[f"{x},{y}"]["probability"] = 0
     return x, y
 
 
@@ -259,6 +285,45 @@ def mark_all_adjacent_unselected(Board, x, y):
             '''
 
 
+def check_rest_probabilities(Board):
+
+    for x in range(0, Board.width):
+        for y in range(0, Board.height):
+            if Board.cell_dict[f"{x},{y}"]["selected"]:
+                adjacent = 0
+                cells_100 = 0
+                cells_0 = 0
+
+                cell_neighbors = [f"{x + 1},{y}", f"{x + 1},{y + 1}", f"{x + 1},{y - 1}", f"{x - 1},{y}",
+                                  f"{x - 1},{y + 1}", f"{x - 1},{y - 1}", f"{x},{y + 1}", f"{x},{y - 1}"]
+
+                for neighbor in cell_neighbors:
+                    if neighbor in Board.cell_dict.keys():
+                        if not Board.cell_dict[neighbor]["selected"]:
+                            adjacent += 1
+                        if not Board.cell_dict[neighbor]["selected"] and Board.cell_dict[neighbor]["probability"] == 1:
+                            cells_100 += 1
+                        if not Board.cell_dict[neighbor]["selected"] and Board.cell_dict[neighbor]["probability"] == 0:
+                            cells_0 += 1
+
+                print("adjacent:", adjacent)
+                if adjacent == 0:
+                    continue
+                else:
+                    probability_of_rest_adjacents = (Board.cell_dict[f"{x},{y}"]["value"]) / (adjacent)
+
+                for neighbor in cell_neighbors:
+                    if neighbor in Board.cell_dict.keys():
+                        if not Board.cell_dict[neighbor]["selected"] and not Board.cell_dict[neighbor]["probability"] == 1 and not Board.cell_dict[neighbor]["probability"] == 0:
+                            if probability_of_rest_adjacents > Board.cell_dict[neighbor]["probability"]:
+                                Board.cell_dict[neighbor]["probability"] = probability_of_rest_adjacents
+
+
+def reset_probabilities(Board):
+    for x in range(0, Board.width):
+        for y in range(0, Board.height):
+            if not Board.cell_dict[f"{x},{y}"]["selected"]:
+                Board.cell_dict[f"{x},{y}"]["probability"] = 0.01
 
 def checks_all_possible_selections(Board):
     canBeSelected = []
